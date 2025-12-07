e8e63c255533a38f42fece9ca3bb4a1ba83aab77
ivanrybkin <ivanforappmail@gmail.com>
2025-12-07 18:43:38 +0100

    calculate_formula

---
+++ conftest.py
@@ -0,0 +1,22 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default= None,
+                     help='Choose browser language')
+
+@pytest.fixture
+def browser(request):
+    user_language = request.config.getoption('--language')
+    options = Options()
+    options.add_experimental_option("prefs", {'intl.accept_languages' : user_language})
+    driver = webdriver.Chrome(options=options)
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    driver.get(link)
+
+    yield driver
+    driver.quit()
+
+
+
